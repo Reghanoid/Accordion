1c5b7638cf14ec37f1148b9f8469e440f4ebca70
Reghanoid <75911479+Reghanoid@users.noreply.github.com>
2022-08-28 17:28:52 +0100

    second commit

--- .history/src/Question_20220606150058.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react'
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai'
-const Question = (props) => {
-  return (
-    <
-  )
-}
-
-export default Question
